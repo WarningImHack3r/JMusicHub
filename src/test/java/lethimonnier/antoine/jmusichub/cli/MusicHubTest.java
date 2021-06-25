e20c732ae39ba264506902c878188fd808b7028e
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2021-06-25 17:52:52 +0200

    Add placebo test

---
+++ src/test/java/lethimonnier/antoine/jmusichub/cli/MusicHubTest.java
@@ -0,0 +1,13 @@
+package lethimonnier.antoine.jmusichub.cli;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MusicHubTest {
+
+	@Test
+	void a() {
+		assertEquals(2, 1 + 1);
+	}
+}
