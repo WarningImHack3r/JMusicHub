470d2041786bb44720154bf163b62084b7a43265
reuben-alt <rubenhattab@gmail.com>
2021-06-27 01:15:20 +0200

    player can now stop and play async

--- src/main/java/lethimonnier/antoine/jmusichub/cli/player/MusicPlayer.java
+++ src/main/java/lethimonnier/antoine/jmusichub/cli/player/MusicPlayer.java
@@ -5,7 +5,7 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 import javazoom.jl.player.advanced.PlaybackEvent;
 import javazoom.jl.player.advanced.PlaybackListener;
 import lethimonnier.antoine.jmusichub.cli.interfaces.AudioContent;
-//import lethimonnier.antoine.jmusichub.cli.logging.MusicLogger;
+import lethimonnier.antoine.jmusichub.cli.logging.MusicLogger;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,48 +13,63 @@ import java.util.logging.Logger;
 
 public class MusicPlayer {
 
+    private final Logger logger = MusicLogger.getLogger("output.log");
     private static MusicPlayer instance;
     private AudioContent currentSong;
+    private Thread playingThread;
     private AdvancedPlayer player;
-    private final Logger logger = Logger.getGlobal();
-    private int frame = 0;
 
-    public static MusicPlayer getPlayer() {
+    public static synchronized MusicPlayer getPlayer() {
         if (instance == null) {
             instance = new MusicPlayer();
         }
         return instance;
     }
 
+    public MusicPlayer setPlayingSong(AudioContent audioContent) {
+        currentSong = audioContent;
+        return this;
+    }
+
     public void play() {
-//        if (currentSong == null) {
-//            logger.severe("No song to play");
-//            return;
-//        }
+        /*if (currentSong == null) {
+            logger.severe("No song to play");
+            return;
+        }*/
         try {
-            player = new AdvancedPlayer(new FileInputStream("Blinding_Lights.mp3"));
+            player = new AdvancedPlayer(new FileInputStream("sample.mp3"));
             player.setPlayBackListener(new PlaybackListener() {
                 @Override
                 public void playbackStarted(PlaybackEvent evt) {
-                    // do something?
+                    logger.info("started");
                 }
 
                 @Override
                 public void playbackFinished(PlaybackEvent evt) {
-                    frame = evt.getFrame();
+                    logger.info("finished");
                 }
             });
-            player.play(frame, Integer.MAX_VALUE);
+            playingThread = new Thread(() -> {
+                try {
+                    player.play();
+                } catch (JavaLayerException e) {
+                    e.printStackTrace();
+                }
+            });
+            playingThread.start();
+            logger.info("Player: Started");
         } catch (FileNotFoundException | JavaLayerException e) {
             e.printStackTrace();
         }
     }
 
-    public void pause() {
-        logger.info("Player: Paused " + currentSong.title);
-    }
-
     public void stop() {
+        /*if (currentSong == null) {
+            logger.severe("No song to stop");
+            return;
+        }*/
+        if (playingThread.isAlive())
+            player.stop();
         logger.info("Player: Stopped");
     }
 }
